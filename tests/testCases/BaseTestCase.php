e8c78e2ac7fe61deeadd2ebd27d1681fb2e7a342
Alessandro Galli <alessandro.galli@facile.it>
2015-08-13 15:50:38 +0200

    base test case

---
+++ tests/testCases/BaseTestCase.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Test\Cases;
+
+/**
+ * Class BaseTestCase
+ * @package Test\Cases
+ */
+class BaseTestCase extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * Sets up the fixture, for example, open a network connection.
+     * This method is called before a test is executed.
+     */
+    protected function setUp()
+    {
+    }
+
+    /**
+     * Teardown per tutti i test
+     */
+    protected function tearDown()
+    {
+
+    }
+
+}
